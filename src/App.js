fbd85b9b62704ff496d625c35f7c94214146d160
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-11-23 12:51:30 -0600

    minor change to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import useLocalStorage from 'use-local-storage'
-import { Row, Col, Card, Container } from 'react-bootstrap'
 import './App.css'
 import Home from './Pages/Home'
 
